63799a9ff84c15c9a0e03456b88e583e6636f760
gabyferr <70179462+gabyferr@users.noreply.github.com>
2022-08-07 00:33:57 -0300

    Melhorias home

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -11,30 +11,35 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawerEnableOpenDragGesture: false,
       appBar: AppBar(
         backgroundColor: Colors.indigo,
         toolbarHeight: 82,
-        leading: Padding(
-          padding: const EdgeInsets.only(left: 12),
-          child: CircleAvatar(
-            radius: 26,
-            child: Image.asset(
-              'assets/img/logo.png',
+        leading: Builder(builder: (context) {
+          return IconButton(
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+            icon: Icon(
+              Icons.list,
+              color: Colors.white,
+              size: 36,
             ),
-          ),
-        ),
+            padding: EdgeInsets.only(left: 26),
+          );
+        }),
         title: Container(
           padding: const EdgeInsets.all(8.0),
           child: Row(
             children: [
-              IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.list,
-                  color: Colors.white,
-                  size: 36,
+              Padding(
+                padding: const EdgeInsets.only(right: 22),
+                child: CircleAvatar(
+                  radius: 26,
+                  child: Image.asset(
+                    'assets/img/logo.png',
+                  ),
                 ),
-                padding: EdgeInsets.only(right: 28),
               ),
               Expanded(
                 child: TextField(
@@ -66,62 +71,109 @@ class _HomePageState extends State<HomePage> {
         ),
         actions: [
           if (MediaQuery.of(context).size.width > 800) ...[
-            Row(
-              children: [
-                CircleAvatar(
-                  child: FlutterLogo(),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: Text(
-                    'Faça seu login.\nOu cadastre-se!',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
+            InkWell(
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.login,
+                    color: Colors.white,
+                    size: 28,
                   ),
-                )
-              ],
+                  Padding(
+                    padding: const EdgeInsets.only(left: 6, right: 18),
+                    child: Text(
+                      'Faça seu login.\nOu cadastre-se!',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              onTap: () => abrirModalLogin(),
             ),
           ],
-          IconButton(
-            padding: EdgeInsets.only(right: 16),
-            onPressed: () {},
-            icon: Icon(
-              Icons.shopping_cart_outlined,
-              size: 32,
-              color: Colors.white,
-            ),
-          )
+          Builder(
+            builder: (context) {
+              return IconButton(
+                padding: EdgeInsets.only(right: 26),
+                onPressed: () {
+                  Scaffold.of(context).openEndDrawer();
+                },
+                icon: Icon(
+                  Icons.shopping_cart_outlined,
+                  size: 32,
+                  color: Colors.white,
+                ),
+              );
+            },
+          ),
         ],
       ),
-      drawer: Drawer(),
-      body: GridView.builder(
-        itemCount: 8,
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 160,
-          crossAxisSpacing: 8,
-        ),
-        itemBuilder: (context, index) => GestureDetector(
-          child: Card(
-            elevation: 7,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Wrap(
-                children: [
-                  FlutterLogo(
-                    size: 80,
-                  ),
-                  Text(
-                    'produto',
-                    style: TextStyle(fontSize: 18),
-                  ),
+      drawer: abrirDrawerMenu(),
+      endDrawer: abrirDrawerCarrinho(),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Colors.blueAccent,
+                  Colors.white,
                 ],
               ),
             ),
           ),
-          onTap: () => abrirModalDetalheProduto(),
-        ),
+          GridView.builder(
+            itemCount: 18,
+            padding: EdgeInsets.all(12),
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 160,
+              crossAxisSpacing: 8,
+              mainAxisSpacing: 8,
+            ),
+            itemBuilder: (context, index) => GestureDetector(
+              child: Card(
+                elevation: 7,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Wrap(
+                    children: [
+                      FlutterLogo(
+                        size: 80,
+                      ),
+                      Text(
+                        'produto',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              onTap: () => abrirModalDetalheProduto(),
+            ),
+          ),
+        ],
       ),
+    );
+  }
+
+  void abrirModalLogin() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Login'),
+          content: Wrap(
+            children: [
+              Text('colocar as descricoes.....'),
+            ],
+          ),
+        );
+      },
     );
   }
 
@@ -131,9 +183,44 @@ class _HomePageState extends State<HomePage> {
       builder: (context) {
         return AlertDialog(
           title: Text('Detalhes do produto'),
-          content: Column(children: [Text('descricoess.....')]),
+          content: Wrap(
+            children: [
+              Text('colocar as descricoes.....'),
+            ],
+          ),
         );
       },
     );
+  }
+
+  Widget abrirDrawerMenu() {
+    return Drawer(
+      child: ListView(
+        children: [
+          DrawerHeader(
+            child: Text('Menu'),
+          ),
+          ListTile(
+            title: Text('Login/Cadastro'),
+            onTap: () => abrirModalLogin(),
+          ),
+          ExpansionTile(
+            title: Text('Categorias'),
+            children: [
+              ListTile(
+                title: Text('Acucares'),
+              ),
+              ListTile(
+                title: Text('Bebidas'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget abrirDrawerCarrinho() {
+    return Drawer();
   }
 }
